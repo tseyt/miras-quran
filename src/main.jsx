f567cf438b9e4f22e8b02f0bcf7c93bd2f9f6c1a
Taner Seyt <tanerseyt@gmail.com>
2025-11-20 01:37:02 -0500

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuranHighlighter from './QuranHighlighter.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuranHighlighter />
+  </React.StrictMode>,
+)
